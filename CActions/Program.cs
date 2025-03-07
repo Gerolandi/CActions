462c4c9572d57acce877925f6947db28401b8bd1
Gerolandi <.>
2025-03-07 13:57:47 +0100

    Write simple program

---
+++ CActions/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Calculating square");
+Console.WriteLine("Give width");
+double width = double.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Give height");
+double height = double.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine($"The square are {width * height}");
