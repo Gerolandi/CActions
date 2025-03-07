7dead447035b71fbcbf5be903104658472a2813b
Gerolandi <.>
2025-03-07 15:41:56 +0100

    Create avalonia cross platform app

---
+++ SuperApp/SuperApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SuperApp.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ SuperApp/SuperApp/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace SuperApp.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
